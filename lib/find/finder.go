9065b32ab5e2a9b949c1aa4235f51c910e8a71b1
Virgula0 <angelo.rosa@protonmail.com>
2025-06-08 11:17:40 +0200

    remove windows redundant trimprefix

--- lib/find/finder.go
+++ lib/find/finder.go
@@ -51,7 +51,6 @@ func (f *finder) Run(ctx context.Context, paths []string) ([]FileInfo, error) {
 		var formattedPattern string = pattern
 		if current := utils.CurrentOS(); strings.HasPrefix(pattern, current.RootPath) {
 			rel := strings.TrimPrefix(formattedPattern, current.RootPath)
-			rel = strings.TrimPrefix(rel, `\`) // windows check
 			formattedPattern = rel
 		}
 
